c44b0b31b960a3513bdaa413ff83d2bcf712f028
Edgar Fonseca <fonsecaer94@gmail.com>
2024-04-17 00:35:58 -0700

    made grid so each book added populates screen properly after clicking submit

--- Library.js
+++ Library.js
@@ -15,9 +15,19 @@ function displayBooks() {
     myLibrary.forEach(e => console.log(e));
 }
 
+// buttons to open dialog box and events
 const dialog = document.querySelector('dialog');
 const showDialogBtn = document.querySelector('#newBtn');
+const submitBtn = document.querySelector('input[type="submit"]');
+const gridContainer = document.querySelector('div.grid-container');
 
 showDialogBtn.addEventListener("click", () => {
     dialog.showModal();
+});
+
+submitBtn.addEventListener("click", () => {
+    gridItem = document.createElement('div');
+    gridItem.className= "grid-item";
+    gridContainer.appendChild(gridItem);
+    dialog.close();
 });
