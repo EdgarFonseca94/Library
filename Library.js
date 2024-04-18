78566f3a200efb898d51dd594852cdecf8500b10
Edgar Fonseca <fonsecaer94@gmail.com>
2024-04-17 19:46:18 -0700

    began logic for collecting info and displaying book. Need to finish to show it on card

--- Library.js
+++ Library.js
@@ -11,9 +11,11 @@ function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
 }
 
-function displayBooks() {
-    myLibrary.forEach(e => console.log(e));
-}
+function displayBook() {
+        gridItem = document.createElement('div');
+        gridItem.className= "grid-item";
+        gridContainer.appendChild(gridItem);
+} 
 
 // buttons to open dialog box and events
 const dialog = document.querySelector('dialog');
@@ -25,9 +27,23 @@ showDialogBtn.addEventListener("click", () => {
     dialog.showModal();
 });
 
-submitBtn.addEventListener("click", () => {
-    gridItem = document.createElement('div');
-    gridItem.className= "grid-item";
-    gridContainer.appendChild(gridItem);
-    dialog.close();
+// form actions
+let bookInfo = document.getElementById("bookInfo");
+
+bookInfo.addEventListener("submit", e => {
+    // prevent submit event before grabbing info
+    e.preventDefault();
+
+    // get information from dialog box
+    title = document.getElementById("title").value;
+    author = document.getElementById("author").value;
+    pages = document. getElementById("pages").value;
+    read_status = document.querySelector("input[name='read_status']:checked").value;
+    
+    // Create book,add to library, and display 
+    newBook = new Book(title, author, pages, read_status);
+    addBookToLibrary(newBook);
+    displayBook();
+
+    //dialog.close();
 });
