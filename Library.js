da8de130c5ff262dcaacc7b6e9e2596e956c98ad
Edgar Fonseca <fonsecaer94@gmail.com>
2024-05-02 22:15:41 -0700

    started adding text info to cards

--- Library.js
+++ Library.js
@@ -10,13 +10,43 @@ function Book (title, author, pages, read_status) {
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
 }
+function clearGrid() {
+    gridContainer.innerHTML = "";
+}
 
-function displayBook() {
-        gridItem = document.createElement('div');
-        gridItem.className= "grid-item";
-        gridContainer.appendChild(gridItem);
-} 
+function displayBook(book) {
+    // creates grid item for card
+    gridItem = document.createElement('div');
+    gridItem.className= "grid-item";
+    gridContainer.appendChild(gridItem);
 
+    // title
+    cardTitle = document.createElement('div');
+    cardTitle.className = "cardTitle";
+    cardTitle.innerHTML = "Title: " + book.title;
+    gridItem.appendChild(cardTitle);
+
+    // author
+    cardAuthor = document.createElement('div');
+    cardAuthor.className = "cardAuthor";
+    cardAuthor.innerHTML = "Author: " + book.author;
+    gridItem.appendChild(cardAuthor);
+
+    // pages
+    cardPages = document.createElement('div');
+    cardPages.className = "cardPages";
+    cardPages.innerHTML = "Pages: " + book.pages;
+    gridItem.appendChild(cardPages);
+
+    // read status
+    cardReadStatus = document.createElement('div');
+    cardReadStatus.className = "cardReadStatus";
+    cardReadStatus.innerHTML = "Author: " + book.read_status;
+    gridItem.appendChild(cardReadStatus);
+
+
+}
+ 
 // buttons to open dialog box and events
 const dialog = document.querySelector('dialog');
 const showDialogBtn = document.querySelector('#newBtn');
@@ -34,6 +64,8 @@ bookInfo.addEventListener("submit", e => {
     // prevent submit event before grabbing info
     e.preventDefault();
 
+    clearGrid();
+
     // get information from dialog box
     title = document.getElementById("title").value;
     author = document.getElementById("author").value;
@@ -43,7 +75,10 @@ bookInfo.addEventListener("submit", e => {
     // Create book,add to library, and display 
     newBook = new Book(title, author, pages, read_status);
     addBookToLibrary(newBook);
-    displayBook();
+    
+    myLibrary.forEach((book) => {
+        displayBook(book);
+    });
 
-    //dialog.close();
+    dialog.close();
 });
